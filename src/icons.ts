f9971efaa6b2b312c03aec50a1eef99dedbd6647
Daan van den Berg <mail@daanvandenberg.com>
2021-01-27 20:35:57 +0100

    Add Font Awesome 5

---
+++ src/icons.ts
@@ -0,0 +1,33 @@
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+// Import Light Icons
+import {
+	faClouds,
+	faCloudShowers,
+	faCloudShowersHeavy,
+	faCloudSun,
+	faHumidity,
+	faMonkey,
+	faSlidersV,
+	faSun,
+	faThunderstorm,
+	faTimes,
+	faUmbrella,
+	faWind,
+} from 'fal';
+
+// Export for Font Awesome library
+export const ICONS: IconDefinition[] = [
+	faClouds,
+	faCloudShowers,
+	faCloudShowersHeavy,
+	faCloudSun,
+	faHumidity,
+	faMonkey,
+	faSlidersV,
+	faSun,
+	faThunderstorm,
+	faTimes,
+	faUmbrella,
+	faWind,
+];
--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,11 @@ import weekday from 'dayjs/plugin/weekday';
 import isoWeek from 'dayjs/plugin/isoWeek';
 import { CssBaseline,  } from '@material-ui/core';
 import { ThemeProvider, StylesProvider, jssPreset } from '@material-ui/core/styles';
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { create } from 'jss';
 import compose from 'jss-plugin-compose';
 import App from './App';
+import { ICONS } from './icons';
 import theme from './theme';
 import './database';
 
@@ -16,6 +18,9 @@ dayjs().format();
 dayjs.locale('nl');
 dayjs.extend(weekday);
 dayjs.extend(isoWeek);
+
+// Import Font Awesome 5 icons
+library.add(...ICONS);
 
 // Add support for JSS Compose: https://cssinjs.org/jss-plugin-compose
 const jss = create({
